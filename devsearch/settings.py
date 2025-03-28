39c9fe9af79bedb7f28a4896fc9ee2cecd86e2c1
Daniel B <daniel.m.bassett@outlook.com>
2025-03-28 10:48:44 -0500

    added media files

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'projects',
+    'projects.apps.ProjectsConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -119,9 +119,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+
--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,7 @@ import uuid
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
+    featured_image = models.ImageField(null=True, blank=True, default='images/icon.svg')
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
