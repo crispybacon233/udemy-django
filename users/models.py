5f533361c930792074a822aed3f0efc7c0d5fd12
Daniel B <daniel.m.bassett@outlook.com>
2025-03-30 12:27:17 -0500

    john daker's face

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
     name = models.CharField(max_length=500, null=True, blank=True)
+    location = models.CharField(max_length=500, blank=True, null=True)
     email = models.EmailField(max_length=500, null=True, blank=True)
     short_intro = models.CharField(max_length=300, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
@@ -25,3 +26,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
