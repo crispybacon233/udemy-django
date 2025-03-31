fdd92285603689505076bde56aaa9bd2f071c846
Daniel <daniel.m.bassett@outlook.com>
2025-03-30 19:12:39 -0500

    pill styling

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.profiles_page, name='profiles-page')
+    path('', views.profiles_page, name='profiles-page'),
+    path('profile/<str:pk>', views.user_profile_page, name='user-profile-page')
 ]
--- users/views.py
+++ users/views.py
@@ -5,3 +5,8 @@ from .models import Profile
 def profiles_page(request):
     profiles = Profile.objects.all()
     return render(request, 'users/profiles.html', {'profiles': profiles})
+
+
+def user_profile_page(request, pk):
+    profile = Profile.objects.get(id=pk)
+    return render(request, 'users/user-profile.html', {'profile': profile})
