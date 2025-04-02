f936ccbc51ef307c75c6e4d29816726957b3fc4a
Daniel B <daniel.m.bassett@outlook.com>
2025-04-02 08:16:40 -0500

    started work on login

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path('login/', views.login_page, name='login'),
     path('', views.profiles_page, name='profiles-page'),
     path('profile/<str:pk>', views.user_profile_page, name='user-profile-page')
 ]
